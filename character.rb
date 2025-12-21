a8617f5c0bac25e97ea608c0cbc5c1196d57990b
Brett <info@binarybills.com>
2025-12-20 17:16:26 -0800

    Added tier calculations and level calculations to test and uncommented the code

--- character.rb
+++ character.rb
@@ -1,51 +1,11 @@
 require_relative 'tools'
 
-#module TierMath
-	#def tier
-		#@rules["advancement"]["tier"].each_with_index do |threshold, tier|
-			#return tier if level < threshold
-		#end
-		
-		#@rules["advancement"]["tier"].length
-	#end
-	#def tier_damage_reduction(); @rules["tier"]["damage_reduction"][tier]; end
-	#def tier_damage_resiliance(); @rules["tier"]["damage_resiliance"][tier]; end
-#end
-
 #module SkillMath
 	#def combat_pool
 		#pool_math = rules["advancement"]["competency"]["combat_pool"][tier]
 		#combat_pool = pool_math["base"] + (pool_math["inc"] * level)
 		#combat_pool = pool_math["max"] if combat_pool > pool_math["max"]
 		#combat_pool
-	#end
-#end
-
-#module KlassMath
-	#def level(); @data["classes"].map { |klass| klass["level"].to_i }.sum; end
-	#def full_klass(); @data["classes"].map { |klass| "#{klass["class"]} #{klass["level"]}" }.join(', '); end
-
-	#def bab
-		#bab_increases = @data["classes"].map do |klass|
-			#bab_adv_rate = @rules["class_advancement"][klass["class"]]["bab"]
-			#bab_mod = @rules["advancement"]["competency"]["bab_ranks_per_level"][bab_adv_rate - 1]
-			#(klass["level"].to_f * bab_mod[0].to_f / bab_mod[1].to_f).to_i
-		#end
-		#bab_increases.sum
-	#end
-
-	#def mana_from_klasses; @data["classes"].map { |klass| klass["mana"].to_i }.sum; end
-
-	#private
-	#def competency; return @rules["advancement"]["competency"]; end
-	#def klass_rules(klass_name); return @rules["class_advancement"][klass_name]; end
-#
-	#def bab_klass_priority(klass_name); return klass_rules(klass_name)["bab"]; end
-	#def bab_per_level(klass_name); frac = competency["bab_ranks_per_level"][bab_klass_priority(klass_name)]; return frac[0].to_f / frac[1].to_f; end
-
-	#def bab_from_klass(klass_index)
-		#klass_data = @data["classes"][klass_index]
-		#return (klass_data["level"].to_i * bab_per_level(klass_data["class"])).to_i
 	#end
 #end
 
@@ -103,9 +63,49 @@ require_relative 'tools'
 	#end
 #end
 
+module TierMath
+	def tier
+		@rules["advancement"]["tier"].each_with_index do |threshold, tier|
+			return tier if level < threshold
+		end
+		
+		@rules["advancement"]["tier"].length
+	end
+	def tier_damage_reduction(); @rules["tier"]["damage_reduction"][tier]; end
+	def tier_damage_resiliance(); @rules["tier"]["damage_resiliance"][tier]; end
+end
+
+module KlassMath
+	def level(); @data["classes"].map { |klass| klass["level"].to_i }.sum; end
+	def full_klass(); @data["classes"].map { |klass| "#{klass["class"]} #{klass["level"]}" }.join(', '); end
+
+	def bab
+		bab_increases = @data["classes"].map do |klass|
+			bab_adv_rate = @rules["class_advancement"][klass["class"]]["bab"]
+			bab_mod = @rules["advancement"]["competency"]["bab_ranks_per_level"][bab_adv_rate - 1]
+			(klass["level"].to_f * bab_mod[0].to_f / bab_mod[1].to_f).to_i
+		end
+		bab_increases.sum
+	end
+
+	def mana_from_klasses; @data["classes"].map { |klass| klass["mana"].to_i }.sum; end
+
+	private
+	def competency; return @rules["advancement"]["competency"]; end
+	def klass_rules(klass_name); return @rules["class_advancement"][klass_name]; end
+
+	def bab_klass_priority(klass_name); return klass_rules(klass_name)["bab"]; end
+	def bab_per_level(klass_name); frac = competency["bab_ranks_per_level"][bab_klass_priority(klass_name)]; return frac[0].to_f / frac[1].to_f; end
+
+	def bab_from_klass(klass_index)
+		klass_data = @data["classes"][klass_index]
+		return (klass_data["level"].to_i * bab_per_level(klass_data["class"])).to_i
+	end
+end
+
 class CharacterSheet
-  #include TierMath
-  #include KlassMath
+  include TierMath
+  include KlassMath
   #include BaseStatsMath
   #include SkillMath
   attr_reader :rules, :id, :data
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -8,27 +8,115 @@ RSpec.describe CharacterSheet do
 	describe '#CharacterSheet' do
 		context 'Lookup static details' do
 			it 'finds name, and player info' do
-				[{name: 'Joe', player: 'Blow'}, {name: 'Kevin', player: 'Quan'}].each_with_index do |expected, index|
+				[['Joe', 'Blow'], ['Kevin', 'Quan'], ['Bob', 'Bobby'], ['Paul', 'Pauly']].each_with_index do |expected, index|
 					character = CharacterSheet.new(character_list[index])
-					expect(character.name).to eq(expected[:name])
-					expect(character.player).to eq(expected[:player])
+					expect(character.name).to eq(expected[0])
+					expect(character.player).to eq(expected[1])
 				end
 			end
 
 			it 'finds deity' do
-				character = CharacterSheet.new(character_list[0])
-				expect(character.deity).to eq('Gorum')
+				{0 => 'Gorum', 2 => 'Calistria'}.each do |index, expected|
+					character = CharacterSheet.new(character_list[index])
+					expect(character.deity).to eq(expected)
+				end
 			end
 
 			it 'gracefully handles null dieties' do
-				character = CharacterSheet.new(character_list[1])
-				expect(character.deity).to eq(nil)
+				[1, 3].each do |index|
+					character = CharacterSheet.new(character_list[index])
+					expect(character.deity).to eq(nil)
+				end
 			end
 
 			it 'correctly displays race' do
-				['Hill dwarf', 'Human'].each_with_index do |expected_race, index|
+				['Hill dwarf', 'Human', 'High elf', 'Human'].each_with_index do |expected_race, index|
 					character = CharacterSheet.new(character_list[index])
 					expect(character.race).to eq(expected_race)
+				end
+			end
+		end
+	end
+
+	describe '#KlassMath' do
+		context 'Verify level math' do
+			it 'calculates level for single class characters' do
+				{0 => 1, 1 => 8}.each do |index, expected|
+					character = CharacterSheet.new(character_list[index])
+					expect(character.level).to eq(expected)
+				end
+			end
+
+			it 'calculates level for multi class characters' do
+				{2 => 7, 3 => 4}.each do |index, expected|
+					character = CharacterSheet.new(character_list[index])
+					expect(character.level).to eq(expected)
+				end
+			end
+		end
+	end
+
+	describe '#TierMath' do
+		context 'Verify tier math' do
+			it 'calculates tier with example characters' do
+				tier = 0
+				expected_hash = (0..rules['advancement']['tier'].max).to_a.map do |level|
+					while (rules['advancement']['tier'][tier]) && (level >= rules['advancement']['tier'][tier])
+						tier += 1
+					end
+					tier
+				end
+
+				character_list.each do |character_data|
+					character = CharacterSheet.new(character_data)
+					expect(character.tier).to eq(expected_hash[character.level])
+				end
+			end
+
+			it 'calculates tier for each level' do
+				tier = 0
+				expected_hash = (0..rules['advancement']['tier'].max).to_a.map do |level|
+					while (rules['advancement']['tier'][tier]) && (level >= rules['advancement']['tier'][tier])
+						tier += 1
+					end
+					tier
+				end
+
+				(0..rules['advancement']['tier'].max).to_a.each do |level|
+					character = CharacterSheet.new(character_list.sample)
+					allow(character).to receive(:level).and_return(level)
+
+					expect(character.tier).to eq(expected_hash[level])
+				end
+			end
+
+			it 'calculates tier damage reduction with example characters' do
+				character_list.each do |character_data|
+					character = CharacterSheet.new(character_data)
+					expect(character.tier_damage_reduction).to eq(rules['tier']['damage_reduction'][character.tier])
+				end
+			end
+
+			it 'calculates tier damage resiliance with example characters' do
+				character_list.each do |character_data|
+					character = CharacterSheet.new(character_data)
+					expect(character.tier_damage_resiliance).to eq(rules['tier']['damage_resiliance'][character.tier])
+				end
+			end
+
+			it 'calculates tier damage reduction for each level' do
+				(0..rules['advancement']['tier'].max).to_a.each do |level|
+					character = CharacterSheet.new(character_list.sample)
+					allow(character).to receive(:level).and_return(level)
+					expect(character.tier_damage_reduction).to eq(rules['tier']['damage_reduction'][character.tier])
+				end
+			end
+
+			it 'calculates tier damage resiliance for each level' do
+				(0..rules['advancement']['tier'].max).to_a.each do |level|
+					character = CharacterSheet.new(character_list.sample)
+					allow(character).to receive(:level).and_return(level)
+					expect(character.tier_damage_resiliance).to eq(rules['tier']['damage_resiliance'][character.tier])
 				end
 			end
 		end
