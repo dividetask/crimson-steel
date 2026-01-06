92bce479a1912f4f26193f6d38a3dd5b19d8be77
Brett <info@binarybills.com>
2026-01-05 17:17:36 -0800

    Added class bonuses. Bit off more then I can chew. Need to refactor

--- character.rb
+++ character.rb
@@ -32,14 +32,44 @@ class SingleKlassProgress
 
 	def bab(rules); return ranks(rules["class_advancement"][@name]["bab"], rules["advancement"]["competency"]["skill_ranks_per_level"]); end
 
-	def mana_from_klass(rules); rules["class_advancement"][@name]["mana"].to_i * @level; end
+	def mana_max(rules); rules["class_advancement"][@name]["mana"].to_i * @level; end
 	def speed_modifiers(rules); (speed_rules(rules)["class"][@name] || []).sum { |level, bonus| @level >= level.to_i ? bonus.to_i : 0}; end
 	def ability_list(rules); return rules["reference"]["class_abilities"][@name].select { |level, list| @level >= level.to_i }.values.flatten; end
 
+	def damage_reduction(rules); return ability_list(rules).sum { |ability| calc_ability_bonus(rules, ability, "damage_reduction") }; end
+	def damage_resiliance(rules); return ability_list(rules).sum { |ability| calc_ability_bonus(rules, ability, "damage_resiliance") }; end
+	def skill_bonus(skill, rules); return ability_list(rules).sum { |ability| calc_ability_bonus(rules, ability, skill) }; end
+	def save_bonus(attr, rules); return ability_list(rules).sum { |ability| calc_ability_bonus(rules, ability, attr) }; end
+
 	private
+	def calc_ability_bonus(rules, ability, var); calc_active_bonus(rules, ability, var) + calc_passive_bonus(rules, ability, var); end
+	def calc_active_bonus(rules, ability, var); parse_formula(traverse_hash(rules["reference"]["abilities"], [ability, "active", var])); end
+	def calc_passive_bonus(rules, ability, var); parse_formula(traverse_hash(rules["reference"]["abilities"], [ability, "passive", var])); end
+
+	def traverse_hash(hash, key_list)
+		return hash if key_list.empty?
+		return 0 unless hash[key_list[0]]
+		return traverse_hash(hash[key_list[0]], key_list[1..-1])
+	end
+
 	def skill_adv_rate(skill, rules); return is_class_skill(skill, rules) ? 3 : 2; end
 	def ranks(adv_rate, adv_rules); mod = adv_rules[adv_rate - 1]; return (@level.to_f * mod[0].to_f / mod[1].to_f).to_i; end
 	def speed_rules(rules); return rules["reference"]["speed_modifiers"]; end
+
+	def parse_formula(formula)
+		return 0 if formula == 0 or formula == nil
+		result = formula.dup
+
+		{level: :level}.each do |key, func_sym|
+			if func_sym.is_a?(Symbol)
+				result.gsub!(key.to_s, send(func_sym).to_s)
+			elsif func_sym.is_a?(Integer)
+				result.gsub!(key.to_s, func_sym.to_s)
+			end
+		end
+		
+		eval(result)
+	end
 end
 
 module KlassProgress
@@ -54,7 +84,11 @@ module KlassProgress
 	def save_total(attr); return save_ranks(attr) + half_mod(attr); end
 	def save_ranks(attr); return @klass_list.sum { |progress| progress.save_ranks(attr, @rules) }; end
 	def save_dice(attr); parse_formula(@rules["advancement"]["competency"]["save_dice"], {"ranks" => save_total(attr)}); end
-	def save_bonus(attr); parse_formula(@rules["advancement"]["competency"]["save_bonus"], {"ranks" => save_total(attr)}); end
+	def save_bonus(attr)
+		base = parse_formula(@rules["advancement"]["competency"]["save_bonus"], {"ranks" => save_total(attr)})
+		class_bonus = @klass_list.sum { |progress| progress.save_bonus(attr, @rules) }
+		return base + class_bonus
+	 end
 
 	def clean_skill_name(skill); return skill.gsub('_', ' ').split(' ').map(&:capitalize).join(' '); end
   
@@ -63,7 +97,11 @@ module KlassProgress
 	def skill_ranks(skill); return @klass_list.sum { |progress| progress.skill_ranks(skill, @rules) }; end
 	def skill_list(); return @klass_list.map { |progress| progress.skill_list }.flatten; end
 	def skill_dice(skill); parse_formula(@rules["advancement"]["competency"]["skill_dice"], {"ranks" => skill_total(skill)}); end
-	def skill_bonus(skill); parse_formula(@rules["advancement"]["competency"]["skill_bonus"], {"ranks" => skill_total(skill)}); end
+	def skill_bonus(skill)
+		base = parse_formula(@rules["advancement"]["competency"]["skill_bonus"], {"ranks" => skill_total(skill)})
+		class_bonus = @klass_list.sum { |progress| progress.skill_bonus(skill, @rules) }
+		return base + class_bonus
+	 end
 
 	def bab; return @klass_list.sum { |progress| progress.bab(@rules) }; end
 	def bab_total; return bab + half_mod(:dex); end
@@ -74,10 +112,13 @@ module KlassProgress
 	def attack_bonus(weapon_bonus); parse_formula(@rules["advancement"]["competency"]["skill_bonus"], {"ranks" => bab_total}) + weapon_bonus; end
 
 	def full_klass(); @data["classes"].map { |klass| "#{klass["class"]} #{klass["level"]}" }.join(', '); end
-	def mana_from_klasses; return @klass_list.sum { |progress| progress.mana_from_klass(@rules) }; end
+	def mana_max; return @klass_list.sum { |progress| progress.mana_max(@rules) } + (defined?(super) ? super : 0); end
 
 	def speed_modifiers; return @klass_list.sum { |progress| progress.speed_modifiers(@rules) } + (defined?(super) ? super : 0); end
 	def ability_list; return @klass_list.map { |progress| progress.ability_list(@rules) }.flatten; end
+
+	def damage_reduction(); return @klass_list.sum { |progress| progress.damage_reduction(@rules) } + (defined?(super) ? super : 0); end
+	def damage_resiliance(); return @klass_list.sum { |progress| progress.damage_resiliance(@rules) } + (defined?(super) ? super : 0); end
 end
 
 module SkillMath
@@ -113,14 +154,16 @@ module BaseStatsMath
 	def attr_bonus(attr); parse_formula(@rules["advancement"]["competency"]["attribute_bonus"], {"attr" => attr}); end
 
 	def hp_max; return parse_formula(@rules["advancement"]["natural"]["hp"][tier]); end
-	def mana_max; return parse_formula(@rules["advancement"]["natural"]["mana"][tier]) + mana_from_klasses; end
+	#def mana_max; return parse_formula(@rules["advancement"]["natural"]["mana"][tier]) + mana_from_klasses; end
+	def mana_max; return parse_formula(@rules["advancement"]["natural"]["mana"][tier]) + (defined?(super) ? super : 0); end
 	def mana_regen; return (mana_max / 4).to_i; end
 end
 
 module TierMath
 	def tier; @rules["advancement"]["tier"].find_index { |threshold| level < threshold } || @rules["advancement"]["tier"].length; end
-	def tier_damage_reduction(); @rules["tier"]["damage_reduction"][tier]; end
-	def tier_damage_resiliance(); @rules["tier"]["damage_resiliance"][tier]; end
+	def tier_damage_reduction(attacker_tier); r = @rules["tier"]["damage_reduction"]; [0, r[tier] - r[attacker_tier]].min; end
+	def damage_reduction(); return 0; end
+	def damage_resiliance(); return @rules["tier"]["damage_resiliance"][tier] + (defined?(super) ? super : 0); end
 end
 
 module CharacterEquipment
@@ -129,9 +172,27 @@ module CharacterEquipment
 	def refresh_items; @item_list = @all_items.select { |item| item["owner_id"].to_i == @id }; end
 	def weapon_list; return @item_list.select { |item| item["type"] == "weapon" }; end
 	def shield_list; return @item_list.select { |item| item["type"] == "shield" }; end
+	def equipped_list; return @item_list.select { |item| item["equipped"] == true }; end
+	def ammunition; return @item_list.select { |item| item["properties"]["ammunition"] == true }; end
+	def consumable; return @item_list.select { |item| item["properties"]["consumable"] == true }; end
 
 	def weapon_dice(weapon_data); attack_dice(weapon_data["bonus"]); end
 	def weapon_attack_bonus(weapon_data); attack_bonus(weapon_data["bonus"]); end
+
+	def damage_reduction()
+		armor = find_item("armor"); 
+		dr = armor ? {"light" => 1, "medium" => 3, "heavy" => 6}[armor["subtype"]].to_i + armor["bonus"].to_i : 0
+		return dr + (defined?(super) ? super : 0)
+	end
+
+	def damage_resiliance()
+		armor = find_item("armor"); 
+		dr = armor ? {"light" => 1, "medium" => 2, "heavy" => 3}[armor["subtype"]].to_i * armor["bonus"].to_i : 0
+		return dr + (defined?(super) ? super : 0)
+	end
+
+	private
+	def find_item(type); return @item_list.find { |item| item["type"] == type }; end  #Needs to add for armor
 end
 
 class CharacterSheet
@@ -155,10 +216,12 @@ class CharacterSheet
 	def race; @data["race"].reverse.join(' ').capitalize; end
 	def race_sym; return (@data["race"][0] || @data["race"]).to_sym; end
 	def speed; return 30 + @rules["reference"]["speed_modifiers"]["race"][race_sym.to_s].to_i + speed_modifiers; end
-	def speed_modifiers; return 0 + super; end
 
-	def damage_reduction(); tier_damage_reduction; end  #Needs to add for armor
-	def damage_resiliance(); tier_damage_resiliance; end  #Needs to add for armor
+	def speed_modifiers; return 0 + super; end
+	def mana_max; return 0 + super; end
+	def damage_reduction(); return 0 + super; end  
+	def damage_resiliance(); return 0 + super; end
+
 	def add_plus(func, params = nil); r = params ? send(func, params) : send(func); return "#{'+' if r >= 0}#{r}"; end
 
 	private
@@ -178,4 +241,3 @@ class CharacterSheet
 		eval(result)
 	end
 end
-
